0474a53007e6a91260fb9eec93283c9c3081aa27
Richard Sivak <sivak.r101@gmail.com>
2025-04-10 00:06:02 -0400

    working copy

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router"
 
 function Header() {
   return (
     <header>
-      <h1>FERA</h1>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/clubs">Clubs</Link></li>
+        </ul>  
+      </nav>
     </header>
   )
 }
